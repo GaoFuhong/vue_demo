d32c7484eed51d4c14d1397d3cc54fe2d47572d3
e <1350086369@qq.com>
2020-03-05 22:11:12 +0800

    vue demo

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add :addComment = "addComment"/>
+      <List :comments = "comments" :deleteComment="deleteComment"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from './components/Add.vue'
+import List from './components/List.vue'
+export default {
+  components: {
+    Add,
+    List
+  },
+  props: ['index'],
+  data () {
+    return {
+      comments: [ // 数据在哪个组件，更新数据的方法就在哪个组件
+        {name: 'Bob', content: 'Vue 难啊！'},
+        {name: 'Jack', content: 'Vue so easy！'},
+        {name: 'Rose', content: 'Vue 还行！'}
+      ]
+    }
+  },
+  methods: {
+    // 添加评论
+    addComment (comment) {
+      this.comments.unshift(comment)
+    },
+    // 删除评论
+    deleteComment (index) {
+      this.comments.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src_base/main.js
@@ -0,0 +1,11 @@
+// 入口JS
+import Vue from 'vue'
+import App from './App.vue'
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>>'
+})
